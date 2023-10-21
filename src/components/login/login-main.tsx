634ce534483abeeac78927fcaa29ae480287962d
FNaF1Door <148506902+FNaF1Door@users.noreply.github.com>
2023-10-21 01:30:08 +0100

    Update login-main.tsx

--- src/components/login/login-main.tsx
+++ src/components/login/login-main.tsx
@@ -12,7 +12,7 @@ export function LoginMain(): JSX.Element {
         <NextImage
           imgClassName='object-cover'
           blurClassName='bg-accent-blue'
-          src='https://flodwebuploadsui.itsmedxmon.repl.co/Flod.png/'
+          src='https://flodwebuploadsui.itsmedxmon.repl.co/Flod.png'
           alt='Flod banner'
           layout='fill'
           useSkeleton
